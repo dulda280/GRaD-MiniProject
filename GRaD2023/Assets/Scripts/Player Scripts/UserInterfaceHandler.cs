8385186c45b3b6e1c7bf6e43de770eb8d7730b5e
tsta19 <tsta19@student.aau.dk>
2023-05-08 18:17:55 +0200

    changes?!

--- GRaD2023/Assets/Scripts/Player Scripts/UserInterfaceHandler.cs
+++ GRaD2023/Assets/Scripts/Player Scripts/UserInterfaceHandler.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UserInterfaceHandler : MonoBehaviour
 {
     public GameObject shopPanel;
+    public GameObject escapeMenu;
+    public Button quitToMenuButton;
+    public Button continueButton;
+
+    private bool escapeMenuActive = false;
 
     public TextMeshProUGUI physicalHealth;
     public TextMeshProUGUI mentalHealth;
@@ -28,6 +34,21 @@ public class UserInterfaceHandler : MonoBehaviour
     void Update()
     {
         UpdatePlayerUI();
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!escapeMenuActive)
+            {
+                escapeMenuActive = true;
+                escapeMenu.SetActive(true);
+                Time.timeScale = 0;
+            } else
+            {
+                escapeMenuActive = false;
+                escapeMenu.SetActive(false);
+                Time.timeScale = 1;
+            }
+        }
+       
     }
 
     public void ShowShopInterface(bool state)
@@ -42,5 +63,17 @@ public class UserInterfaceHandler : MonoBehaviour
         mentalHealth.text = Mathf.Ceil(player.mental).ToString();
         hungerAndThirst.text = Mathf.Ceil(player.hungerAndThirst).ToString();
         money.text = player.money.ToString();
+    }
+
+    public void OnClickContinue()
+    {
+        escapeMenuActive = false;
+        escapeMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void OnClickQuitToMenu()
+    {
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
     }
 }
